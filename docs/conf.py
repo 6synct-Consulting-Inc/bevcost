ac3a5f5b537635e9a50b4f4b432981fb0c96b90a
Edward <edwardfagan@fastmail.com>
2026-01-13 09:11:12 -0500

    Basic docs starting point

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'bevcost'
-copyright = 'Zero Nexus Inc.'
-author = 'Edward Fagan'
-release = '0.0.1'
+project = 'Example'
+copyright = 'workshop participant'
+author = 'workshop participant'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -24,5 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
